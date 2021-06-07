16dfe79065f4bd8d644c86052393b22c95f22a30
Hugo de Haan <hugo_dehaan@hotmail.nl>
2021-06-07 10:54:04 +0200

    detailpagina

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,12 +8,15 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApplication1.Models;
 using MySql.Data.MySqlClient;
+using SendAndStore.Models;
 
 namespace WebApplication1.Controllers
 {
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		// stel in waar de database gevonden kan worden
+		string connectionString = "Server=172.16.160.21;Port=3306;Database=110272;Uid=110272;Pwd=inf2021sql;";
 
 		public HomeController(ILogger<HomeController> logger)
 		{
@@ -31,8 +34,7 @@ namespace WebApplication1.Controllers
 
 		public List<string> GetNames()
 		{
-			// stel in waar de database gevonden kan worden
-			string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110272;Uid=110272;Pwd=inf2021sql;";
+		
 
 			// maak een lege lijst waar we de namen in gaan opslaan
 			List<string> names = new List<string>();
@@ -78,12 +80,39 @@ namespace WebApplication1.Controllers
 			return View();
 		}
 
-		[Route("festival/{id}")]
-		public IActionResult festival(string id)
+		[Route("Festival/{id}")]
+		public IActionResult Festival(string id)
 		{
-			return View();
+			var model = GetFestival(id);
+			return View(model);
+		}
 
+		private Festival GetFestival(string id)
+		{
+			List<Festival> festival = new List<Festival>();
 
+			using (MySqlConnection conn = new MySqlConnection(connectionString))
+			{
+				conn.Open();
+				MySqlCommand cmd = new MySqlCommand($"select * from festival where id = {id}", conn);
+
+				using (var reader = cmd.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						Festival p = new Festival
+						{
+							Id = Convert.ToInt32(reader["Id"]),
+							Naam = reader["Naam"].ToString(),
+							Beschrijving = reader["Beschrijving"].ToString(),
+							Datum = DateTime.Parse(reader["Datum"].ToString()),
+						};
+						festival.Add(p);
+					}
+				}
+			}
+
+			return festival[0];
 		}
 
 		[Route("contact")]
@@ -108,5 +137,9 @@ namespace WebApplication1.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+
+
+		
 	}
 }
+
---
+++ Models/Festival.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SendAndStore.Models
+{
+    public class Festival
+    {
+
+        public string Beschrijving { get; internal set; }
+        public string Naam { get; internal set; }
+        public DateTime Datum { get; internal set; }
+        public int Id { get; internal set; }
+    }
+}
