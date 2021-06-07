24f2b22863cbc6cebee3d3220f7d79d10ca8ff61
Hugo de Haan <hugo_dehaan@hotmail.nl>
2021-06-07 18:06:36 +0200

    Basis van Contactformulier is af

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SendAndStore.Models
 {
     public class Person
     {
+        [Required(ErrorMessage = "Voornaam is verplicht in te vullen")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Achternaam is verplicht in te vullen")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is verplicht in te vullen")]
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Adress { get; set; }
